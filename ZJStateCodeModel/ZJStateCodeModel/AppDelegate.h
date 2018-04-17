bb1ffd009c1ba23120b375c38bbe12abca856270
zhenjin <zhenjin@staff.sina.com.cn>
2018-04-17 17:45:14 +0800

    创建项目

---
+++ ZJStateCodeModel/ZJStateCodeModel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZJStateCodeModel
+//
+//  Created by zhenjin on 2018/4/17.
+//  Copyright © 2018年 zhenjin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
